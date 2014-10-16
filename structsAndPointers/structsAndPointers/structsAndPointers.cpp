6926943da61463d248b2ab2982693618de63da75
Russell <sell_nat@yahoo.com>
2014-10-16 11:03:00 -0700

    Replaced reassignment with a copy for the original class

--- structsAndPointers/structsAndPointers/structsAndPointers.cpp
+++ structsAndPointers/structsAndPointers/structsAndPointers.cpp
@@ -117,7 +117,7 @@ void sortGrades(Student students[])
 	}
 
 }
-
+/*
 void assignClass(Student theClass[])
 {
 	theClass[0].grade = 67;
@@ -165,11 +165,13 @@ void assignClass(Student theClass[])
 	theClass[9].bday = myDate(6, 1, 2013);
 
 }
+*/
 
 
 int main()
 {
 	Student theClass[10];
+	Student classCopy[10];
 	Student *byGrade[10];
 	Student *byName[10];
 	Student *byId[10];
@@ -178,7 +180,49 @@ int main()
 	int choice = 0;
 	int k;
 
-	assignClass(theClass);
+	theClass[0].grade = 67;
+	theClass[1].grade = 70;
+	theClass[2].grade = 98;
+	theClass[3].grade = 75;
+	theClass[4].grade = 50;
+	theClass[5].grade = 89;
+	theClass[6].grade = 80;
+	theClass[7].grade = 75;
+	theClass[8].grade = 55;
+	theClass[9].grade = 57;
+
+	strcpy_s(theClass[0].name, "Russell");
+	strcpy_s(theClass[1].name, "Tom");
+	strcpy_s(theClass[2].name, "John");
+	strcpy_s(theClass[3].name, "Sam");
+	strcpy_s(theClass[4].name, "Paul");
+	strcpy_s(theClass[5].name, "Mark");
+	strcpy_s(theClass[6].name, "Lisa");
+	strcpy_s(theClass[7].name, "Bailey");
+	strcpy_s(theClass[8].name, "Sarah");
+	strcpy_s(theClass[9].name, "Tina");
+
+	theClass[0].id = 1005;
+	theClass[1].id = 5672;
+	theClass[2].id = 8372;
+	theClass[3].id = 9275;
+	theClass[4].id = 4444;
+	theClass[5].id = 6719;
+	theClass[6].id = 2367;
+	theClass[7].id = 1025;
+	theClass[8].id = 7890;
+	theClass[9].id = 4242;
+
+	theClass[0].bday = myDate(10, 2, 1992);
+	theClass[1].bday = myDate(7, 16, 1994);
+	theClass[2].bday = myDate(9, 27, 1997);
+	theClass[3].bday = myDate(8, 12, 1998);
+	theClass[4].bday = myDate(5, 9, 2005);
+	theClass[5].bday = myDate(3, 24, 2000);
+	theClass[6].bday = myDate(12, 25, 2008);
+	theClass[7].bday = myDate(11, 18, 2012);
+	theClass[8].bday = myDate(2, 19, 2014);
+	theClass[9].bday = myDate(6, 1, 2013);
 
 	for (k = 0; k < 10; k++)
 	{
@@ -186,6 +230,7 @@ int main()
 		byName[k] = &theClass[k];
 		byId[k] = &theClass[k];
 		byBday[k] = &theClass[k];
+		classCopy[k] = theClass[k];
 	}
 
 
@@ -205,8 +250,7 @@ int main()
 		switch (choice)
 		{
 		case 1:
-			assignClass(theClass);
-			display(theClass);
+			display(classCopy);
 			
 			break;
 		case 2:
