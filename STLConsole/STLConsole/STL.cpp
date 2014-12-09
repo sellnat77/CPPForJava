3a9b0c40e4bbaa91a32f7a15fedda6be0dbf0a40
sellnat77 <sell_nat@yahoo.com>
2014-12-08 20:56:15 -0800

    Finished with stl project, need to format

--- STLConsole/STLConsole/STL.cpp
+++ STLConsole/STLConsole/STL.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <stdio.h>
-#include <list>
+#include <set>
 
 using namespace std;
 
 int main()
 {
-	list<int> mySet;
-	list<int>::iterator walk;
+	multiset<int> mySet;
+	multiset<int>::iterator walk;
 	int k;
 	int sum = 0;
 	int rando;
@@ -15,18 +15,18 @@ int main()
 	for (k = 0; k < 10000; k++)
 	{
 		rando = (int)((rand() %100)+1);
-		mySet.push_front(rando);
+		mySet.insert(rando);
 	}
-	mySet.sort();
+	//mySet.sort();
 	k = 0;
 	for (walk = mySet.begin(); walk != mySet.end(); walk++)
 	{
-		if (k == 19 )
+		if (k % 5 == 0 )
 		{
-			//cout << "\n";
+			cout << "\n";
 			k = 0;
 		}
-		printf("%1i",*walk);
+		printf("%3i",*walk);
 		sum += *walk;
 		k++;
 	}
@@ -37,6 +37,16 @@ int main()
 	system("pause");
 	cout << "\n\tAverage = " << (int)(sum / 10000) << "\n\n";
 
+
+	for (k = 0; k < 100; k++)
+	{
+		printf("Count of %3i = %3i",(k+1),mySet.count(k + 1));
+		if (k % 5 == 0)
+		{
+			cout << "\n";
+			
+		}
+	}
 	
 
 	return 0;
