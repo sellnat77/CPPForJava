64b566045ec7a45af21dc66f23188ab264fe1c08
sellnat77 <sell_nat@yahoo.com>
2014-12-08 20:43:11 -0800

    Give up

--- MemoryAllocation/MemoryAllocation/MemoryManager.cpp
+++ MemoryAllocation/MemoryAllocation/MemoryManager.cpp
@@ -6,231 +6,231 @@
 
 #include "MemoryManager.h"
 
-namespace MemoryManager
-{
-	const int MM_POOL_SIZE = 65536; //65536
-	char* MM_pool = new char[MM_POOL_SIZE];
-
-	void initializeMemoryManager(void)
+	namespace MemoryManager
 	{
-		int k;
-		//char* MM_pool = new char[MM_POOL_SIZE];
-		for (k = 0; k < MM_POOL_SIZE; k++)
+		const int MM_POOL_SIZE = 65536; //65536
+		char* MM_pool = new char[MM_POOL_SIZE];
+
+		void initializeMemoryManager(void)
 		{
-			MM_pool[k] = 'o';
-			//std::cout << *(unsigned short*)MM_pool+k;
-		}
-
-		//*(unsigned short*)(void*)(MM_pool + 0) = 261;//Total used
-		
-
-		*(unsigned short*)(void*)(MM_pool + 0) = 6;//Total used	
-		*(unsigned short*)(void*)(MM_pool + 1) = 6;//Next free
-		*(unsigned short*)(void*)(MM_pool + 2) = 0;//Prev free
-		//std::cout << "Value of 6 = " << (unsigned short)(MM_pool[0]) << "\n";
-		/*
-		*(unsigned short*)(void*)(MM_pool + 3) = 10;//Total used
-		*(unsigned short*)(void*)(MM_pool + 4) = 16;//Next free
-		*(unsigned short*)(void*)(MM_pool + 5) = 5;//Prev free
-		*/
-		/*
-		*(unsigned short*)(void*)(MM_pool + 8) = 10;//Total used
-		*(unsigned short*)(void*)(MM_pool + 9) = 26;//Next free       
-		*(unsigned short*)(void*)(MM_pool + 10) = 11;//Prev free
-
-		*(unsigned short*)(void*)(MM_pool + 13) = 14;//Total used
-		*(unsigned short*)(void*)(MM_pool + 14) = 40;//Next free       
-		*(unsigned short*)(void*)(MM_pool + 15) = 21;//Prev free
-		*/
-		
-		//for (k = 0; k < 11; k++)
-		//{
-		//	std::cout << "MM_pool[" << k << "] = " << (unsigned short)(MM_pool[k]) << "\n";
-		//}
-		//std::cout << "Value of 6 = " << (unsigned short)(MM_pool[0]);
-		
-	}
-	void outOfMemory(void)
-	{
-		std::cerr << "Memory pool out of memory" << std::endl;
-		exit(1);
-	}
-	void* allocate(int aSize)
-	{
-		void* startOfAllocated = NULL;
-		unsigned short totalUsed;
-		unsigned short nextFree;
-		unsigned short prevFree;
-		unsigned short nextOpen;
-		unsigned short temp;
-		int k;
-
-		nextOpen = (unsigned short)((int)(1.0*(abs(MM_POOL_SIZE - freeRemaining())) / 2));
-		totalUsed = (unsigned short)(aSize + 6);
-		//std::cout << "USING " << (unsigned short)totalUsed << "\n\n\n";
-		//std::cout << "Being stored at " << nextOpen;
-		nextFree = (unsigned short)(totalUsed+(abs(MM_POOL_SIZE - freeRemaining())));
-		prevFree = (unsigned short)((abs(MM_POOL_SIZE - freeRemaining()) - 1));
-
-		*(unsigned short*)(void*)(MM_pool+nextOpen) = totalUsed;
-		temp = *(unsigned short*)(MM_pool + nextOpen);
-		//std::cout << "\n\n\n\n\t" << temp;
-		*(unsigned short*)(void*)(MM_pool + nextOpen+1) = nextFree;
-		*(unsigned short*)(void*)(MM_pool + nextOpen+2) = prevFree;
-		startOfAllocated = (MM_pool + nextOpen + 3);
-
-		
-		//std::cout << "MMPool[" << 8 << "] = " << temp << "\n";
-
-
-		/*
-
-		totalUsed = (unsigned short)(aSize+6);
-		nextOpen = totalUsed / 2;
-		std::cout << "\nValue of 10 = " << totalUsed << "\n\n";
-		nextFree = (unsigned short)((MM_POOL_SIZE - freeRemaining()) + totalUsed);
-		std::cout << "\nValue of 16 = " << nextFree << "\n\n";
-		prevFree = (unsigned short)(MM_POOL_SIZE - freeRemaining()-1);
-		std::cout << "\nValue of 5 = " << prevFree << "\n\n";
-
-
-		std::cout << "\nnext open = " << nextOpen<<"\n\n";
-		*(unsigned short*)(void*)(MM_pool + nextOpen) = totalUsed;
-		*(unsigned short*)(void*)(MM_pool + (nextOpen + 1)) = nextFree;
-		*(unsigned short*)(void*)(MM_pool + (nextOpen + 2)) = prevFree;
-		std::cout << "\n\nvalue of 10 = " << (unsigned short)(MM_pool[(nextOpen+0)]) << "\n\n";
-		std::cout << "\n\nvalue of 16 = " << (unsigned short)(MM_pool[nextOpen+1]) << "\n\n";
-		std::cout << "\n\nvalue of 5 = " << (unsigned short)(MM_pool[nextOpen+2]) << "\n\n";
-		startOfAllocated = (void*)(MM_pool +( nextOpen + 3));
-
-		for (k = 0; k < 5000; k++)
-		{
-			if (k % 3 == 0)
+			int k;
+			//char* MM_pool = new char[MM_POOL_SIZE];
+			for (k = 0; k < MM_POOL_SIZE; k++)
 			{
-				std::cout << "\n";
+				MM_pool[k] = 'o';
+				//std::cout << *(unsigned short*)MM_pool+k;
 			}
-			if ((unsigned short)(MM_pool[k]) == totalUsed)
-			{
-				std::cout << "MM_pool[" << k << "] = " << (unsigned short)(MM_pool[k]) << "\t";
-				break;
-			}
-			std::cout << "MM_pool[" << k << "] = " << (unsigned short)(MM_pool[k]) << "\t";
-		}
+
+			//*(unsigned short*)(void*)(MM_pool + 0) = 261;//Total used
 
 
-		*/
-
-		return startOfAllocated;
-	}
-
-	void deallocate(void* aPointer)
-	{
-		int k=0;
-		unsigned short count = 0;
-		char* point = (char*)aPointer;
-
-		while (k < MM_POOL_SIZE)
-		{
-			
-			count++;
-			k++;
-			//std::cout << "count is " << count << "\n\n";
-		}
-		std::cout << "count is " << count << "\n\n";
-
-	}
-
-	int freeRemaining(void)
-	{
-		int position = 0;
-		int k = 0;
-		unsigned short used = (unsigned short)(MM_pool[position]);
-		//std::cout << "\nUSED = " << used;
-		unsigned short next = (unsigned short)(MM_pool[position+1]);
-		unsigned short prev = (unsigned short)(MM_pool[position+2]);
-		unsigned short count = used;
-		//std::cout << "\n\nStart counting\n";
-		//std::cout << "\nK: " << k;
-		//std::cout << "\nUSE 6: " << used; 
-		//std::cout << "\nCOUNT: " << count;
-		//std::cout << "\nCOUNT/2: " << (int)((1.0*count) / 2);
-		//std::cout << "\nNext: " << next;
-
-		//for (k = 0; k < MM_POOL_SIZE / 500; k++)
-		//{
-		//	std::cout << (unsigned short)(MM_pool[k]);
-		//}
-		
-		k = used;
-
-		while (k < MM_POOL_SIZE)
-		{
-			position = (int)(ceil((1.0*next) / 2));
+			*(unsigned short*)(void*)(MM_pool + 0) = 6;//Total used	
+			*(unsigned short*)(void*)(MM_pool + 1) = 6;//Next free
+			*(unsigned short*)(void*)(MM_pool + 2) = 0;//Prev free
+			//std::cout << "Value of 6 = " << (unsigned short)(MM_pool[0]) << "\n";
 			/*
-			std::cout << "\nK: " << k;
-			std::cout << "\nUSE BEFORE: " << used;
-			std::cout << "\nCOUNT: " << count;
-			std::cout << "\nCOUNT/2: " << count/2;
-			std::cout << "\nNext: " << next;
+			*(unsigned short*)(void*)(MM_pool + 3) = 10;//Total used
+			*(unsigned short*)(void*)(MM_pool + 4) = 16;//Next free
+			*(unsigned short*)(void*)(MM_pool + 5) = 5;//Prev free
 			*/
-			k += next;
-			count += used;
-			//std::cout << "\nCOUNT: " << count;
+			/*
+			*(unsigned short*)(void*)(MM_pool + 8) = 10;//Total used
+			*(unsigned short*)(void*)(MM_pool + 9) = 26;//Next free
+			*(unsigned short*)(void*)(MM_pool + 10) = 11;//Prev free
 
-			used = (unsigned short)MM_pool[position];
-			
-			next = (unsigned short)MM_pool[position + 1];
-			
-			//std::cout << "\nK: " << k;
-			//std::cout << "\nUSE AFTER: " << used;
-			if (used == (unsigned short)((char)('o')))
-			{
-				count -= 6;
-				break;
-			}			
-			
+			*(unsigned short*)(void*)(MM_pool + 13) = 14;//Total used
+			*(unsigned short*)(void*)(MM_pool + 14) = 40;//Next free
+			*(unsigned short*)(void*)(MM_pool + 15) = 21;//Prev free
+			*/
+
+			//for (k = 0; k < 11; k++)
+			//{
+			//	std::cout << "MM_pool[" << k << "] = " << (unsigned short)(MM_pool[k]) << "\n";
+			//}
+			//std::cout << "Value of 6 = " << (unsigned short)(MM_pool[0]);
+
 		}
-		//std::cout << "Used = " << abs(MM_POOL_SIZE-MM_POOL_SIZE-count) << "\n";
-		return MM_POOL_SIZE-count;
-	}
-
-	int largestFree()
-	{
-		return 0;
-	}
-
-	int smallestFree(void)
-	{
-		return 0;
-	}
-
-
-
-	//Call if a pointer is over run condition is detected
-	void onOverrunDetected(void)
-	{
-		std::cerr << "Pointer overrun detected" << std::endl;
-		exit(1);
-	}
-
-	//Call for any other error condition, providing meaningul error messages are appreciated
-	void onIllegalOperation(const char* fmt)
-	{
-		if (fmt == NULL)
+		void outOfMemory(void)
 		{
-			std::cerr << "Unknown illegal operation" << std::endl;
+			std::cerr << "Memory pool out of memory" << std::endl;
 			exit(1);
 		}
-		else
+		void* allocate(int aSize)
 		{
-			char buf[8192];
+			void* startOfAllocated = NULL;
+			unsigned short totalUsed;
+			unsigned short nextFree;
+			unsigned short prevFree;
+			unsigned short nextOpen;
+			unsigned short temp;
+			int k;
 
-			va_list argptr;
-			va_start(argptr, fmt);
-			vsprintf(buf, fmt, argptr);
-			va_end(argptr);
+			nextOpen = (unsigned short)((int)(1.0*(abs(MM_POOL_SIZE - freeRemaining())) / 2));
+			totalUsed = (unsigned short)(aSize + 6);
+			//std::cout << "USING " << (unsigned short)totalUsed << "\n\n\n";
+			//std::cout << "Being stored at " << nextOpen;
+			nextFree = (unsigned short)(totalUsed + (MM_POOL_SIZE - freeRemaining()));
+			prevFree = (unsigned short)((abs(MM_POOL_SIZE - freeRemaining()) - 1));
 
-			std::cerr << "Illegal operation: \"" << buf << "\"" << std::endl;
+			*(unsigned short*)(void*)(MM_pool + nextOpen) = totalUsed;
+			temp = *(unsigned short*)(MM_pool + nextOpen);
+			//std::cout << "\n\n\n\n\t" << temp;
+			*(unsigned short*)(void*)(MM_pool + nextOpen + 1) = nextFree;
+			*(unsigned short*)(void*)(MM_pool + nextOpen + 2) = prevFree;
+			startOfAllocated = (MM_pool + nextOpen + 3);
+
+
+			//std::cout << "MMPool[" << 8 << "] = " << temp << "\n";
+
+
+			/*
+
+			totalUsed = (unsigned short)(aSize+6);
+			nextOpen = totalUsed / 2;
+			std::cout << "\nValue of 10 = " << totalUsed << "\n\n";
+			nextFree = (unsigned short)((MM_POOL_SIZE - freeRemaining()) + totalUsed);
+			std::cout << "\nValue of 16 = " << nextFree << "\n\n";
+			prevFree = (unsigned short)(MM_POOL_SIZE - freeRemaining()-1);
+			std::cout << "\nValue of 5 = " << prevFree << "\n\n";
+
+
+			std::cout << "\nnext open = " << nextOpen<<"\n\n";
+			*(unsigned short*)(void*)(MM_pool + nextOpen) = totalUsed;
+			*(unsigned short*)(void*)(MM_pool + (nextOpen + 1)) = nextFree;
+			*(unsigned short*)(void*)(MM_pool + (nextOpen + 2)) = prevFree;
+			std::cout << "\n\nvalue of 10 = " << (unsigned short)(MM_pool[(nextOpen+0)]) << "\n\n";
+			std::cout << "\n\nvalue of 16 = " << (unsigned short)(MM_pool[nextOpen+1]) << "\n\n";
+			std::cout << "\n\nvalue of 5 = " << (unsigned short)(MM_pool[nextOpen+2]) << "\n\n";
+			startOfAllocated = (void*)(MM_pool +( nextOpen + 3));
+			*/
+			for (k = 50; k<80; k++)
+			{
+				if (k % 3 == 0)
+				{
+					std::cout << "\n";
+				}
+				//if ((unsigned short)(MM_pool[k]) == totalUsed)
+				//{
+				//	std::cout << "MM_pool[" << k << "] = " << (unsigned short)(MM_pool[k]) << "\t";
+				//	break;
+				//}
+				std::cout << "MM_pool[" << k << "] = " << (unsigned short)(MM_pool[k]) << "\t";
+			}
+
+
+
+
+			return startOfAllocated;
+		}
+
+		void deallocate(void* aPointer)
+		{
+			int k = 0;
+			unsigned short count = 0;
+			char* point = (char*)aPointer;
+
+			while (k < MM_POOL_SIZE)
+			{
+
+				count++;
+				k++;
+				//std::cout << "count is " << count << "\n\n";
+			}
+			std::cout << "count is " << count << "\n\n";
+
+		}
+
+		int freeRemaining(void)
+		{
+			int position = 0;
+			int k = 0;
+			unsigned short used = (unsigned short)(MM_pool[position]);
+			//std::cout << "\nUSED = " << used;
+			unsigned short next = (unsigned short)(MM_pool[position + 1]);
+			unsigned short prev = (unsigned short)(MM_pool[position + 2]);
+			unsigned short count = used;
+			//std::cout << "\n\nStart counting\n";
+			//std::cout << "\nK: " << k;
+			//std::cout << "\nUSE 6: " << used; 
+			//std::cout << "\nCOUNT: " << count;
+			//std::cout << "\nCOUNT/2: " << (int)((1.0*count) / 2);
+			//std::cout << "\nNext: " << next;
+
+			//for (k = 0; k < MM_POOL_SIZE / 500; k++)
+			//{
+			//	std::cout << (unsigned short)(MM_pool[k]);
+			//}
+
+			k = used;
+
+			while (k < MM_POOL_SIZE)
+			{
+				position = (int)(ceil((1.0*next) / 2));
+				/*
+				std::cout << "\nK: " << k;
+				std::cout << "\nUSE BEFORE: " << used;
+				std::cout << "\nCOUNT: " << count;
+				std::cout << "\nCOUNT/2: " << count/2;
+				std::cout << "\nNext: " << next;
+				*/
+				k += next;
+				count += used;
+				//std::cout << "\nCOUNT: " << count;
+
+				used = (unsigned short)MM_pool[position];
+
+				next = (unsigned short)MM_pool[(position + 1)];
+
+				//std::cout << "\nK: " << k;
+				//std::cout << "\nUSE AFTER: " << used;
+				if (used == (unsigned short)((char)('o')))
+				{
+					count -= 6;
+					break;
+				}
+
+			}
+			//std::cout << "Used = " << abs(MM_POOL_SIZE-MM_POOL_SIZE-count) << "\n";
+			return MM_POOL_SIZE - (int)count;
+		}
+
+		int largestFree()
+		{
+			return 0;
+		}
+
+		int smallestFree(void)
+		{
+			return 0;
+		}
+
+
+
+		//Call if a pointer is over run condition is detected
+		void onOverrunDetected(void)
+		{
+			std::cerr << "Pointer overrun detected" << std::endl;
+			exit(1);
+		}
+
+		//Call for any other error condition, providing meaningul error messages are appreciated
+		void onIllegalOperation(const char* fmt)
+		{
+			if (fmt == NULL)
+			{
+				std::cerr << "Unknown illegal operation" << std::endl;
+				exit(1);
+			}
+			else
+			{
+				char buf[8192];
+
+				va_list argptr;
+				va_start(argptr, fmt);
+				vsprintf(buf, fmt, argptr);
+				va_end(argptr);
+
+				std::cerr << "Illegal operation: \"" << buf << "\"" << std::endl;
+			}
 		}
 	}
-}
--- MemoryAllocation/MemoryAllocation/main.cpp
+++ MemoryAllocation/MemoryAllocation/main.cpp
@@ -12,24 +12,24 @@ int main(void)
 
 	std::cout << "Initialize uses 6 bytes...expecting 65530\n";
 	initializeMemoryManager();
-	
+
 	long* int_pointer;
 	char* string_pointer;
-
+	/*
 	std::cout << "Free memory = " << freeRemaining() << "\n\n";
 	//std::cout << sizeof(long);
-	
+
 	std::cout << "Allocating size 4 plus additional 6 bytes...expecting 65520\n";
 	int_pointer = (long*)allocate(sizeof(long)); //4
-	
+
 	std::cout << "Free memory = " << freeRemaining() << "\n\n";
-	
+
 	std::cout << "Allocating size 20 plus additional 6 bytes...expecting 65494\n";
 	string_pointer = (char*)allocate(20); //255
 	//std::cout << "size of string pointer" << sizeof(string_pointer);
 
 	//std::cout << "Free memory = " << freeRemaining() << "\n\n";
-	
+
 	//*int_pointer = 0xDEADBEEF;//too big
 
 	//std::cout << "Free memory = " << freeRemaining() << std::endl;
@@ -38,52 +38,63 @@ int main(void)
 	//std::cout << "size of string pointer" << string_pointer[0] << string_pointer[1] << string_pointer[2];
 
 	std::cout << "Free memory = " << freeRemaining() << "\n\n";
-	
+
 	std::cout << "Deallocating size 4 plus 6 bytes...expecting 65504\n";
 	deallocate(int_pointer);
 	std::cout << "Free memory = " << freeRemaining() << "\n\n";
-	
+
 	std::cout << "Deallocating size 20 plus 6 bytes...expecting 65530\n";
 	deallocate(string_pointer);
 	std::cout << "Free memory = " << freeRemaining() << "\n\n";
-	
+	*/
+	// the next several lines are the new lines that I have added for testing
+	int *iptr[20];
+	int i;
+
+	// 10 points if this part of the program works - this for loop
+	for (i = 1; i<15; i++)
+	{
+		iptr[i] = (int*)allocate(sizeof(int));
+		std::cout << "Allocation #" << i << "Free mem = " << freeRemaining() << std::endl;
+	}
+
 
 }
 /*
 namespace MemoryManager
 {
-	void outOfMemory(void)
-	{
-		std::cerr << "Memory pool out of memory" << std::endl;
-		exit(1);
-	}
+void outOfMemory(void)
+{
+std::cerr << "Memory pool out of memory" << std::endl;
+exit(1);
+}
 
-	//Call if a pointer is over run condition is detected
-	void onOverrunDetected(void)
-	{
-		std::cerr << "Pointer overrun detected" << std::endl;
-		exit(1);
-	}
+//Call if a pointer is over run condition is detected
+void onOverrunDetected(void)
+{
+std::cerr << "Pointer overrun detected" << std::endl;
+exit(1);
+}
 
-	//Call for any other error condition, providing meaningul error messages are appreciated
-	void onIllegalOperation(const char* fmt)
-	{
-		if (fmt == NULL)
-		{
-			std::cerr << "Unknown illegal operation" << std::endl;
-			exit(1);
-		}
-		else
-		{
-			char buf[8192];
+//Call for any other error condition, providing meaningul error messages are appreciated
+void onIllegalOperation(const char* fmt)
+{
+if (fmt == NULL)
+{
+std::cerr << "Unknown illegal operation" << std::endl;
+exit(1);
+}
+else
+{
+char buf[8192];
 
-			va_list argptr;
-			va_start(argptr, fmt);
-			vsprintf(buf, fmt, argptr);
-			va_end(argptr);
+va_list argptr;
+va_start(argptr, fmt);
+vsprintf(buf, fmt, argptr);
+va_end(argptr);
 
-			std::cerr << "Illegal operation: \"" << buf << "\"" << std::endl;
-		}
-	}
+std::cerr << "Illegal operation: \"" << buf << "\"" << std::endl;
+}
+}
 }
 */
