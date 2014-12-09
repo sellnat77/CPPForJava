2d2bae619ede6907164cfe6ebcc4e9f6d3eb7efe
Russell Tan <sell_nat@yahoo.com>
2014-12-09 10:44:27 -0800

    Minimized lines within reason

--- STLConsole/STLConsole/STL.cpp
+++ STLConsole/STLConsole/STL.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include <stdio.h>
 #include <time.h>
 #include <set>
-
 using namespace std;
-
 int main()
 {
 	srand(time(NULL));
 	multiset<int> mySet;
 	multiset<int>::iterator walk;
-	int k;
 	int sum = 0;
-	int rando;
+	int rando,k;
 
 	for (k = 0; k < 10000; k++)
 	{
-		rando = (int)((rand() %100)+1);
-		mySet.insert(rando);
+		mySet.insert((int)((rand() % 100) + 1));
 	}
 	k = 0;
 	for (walk = mySet.begin(); walk != mySet.end(); walk++)
@@ -31,25 +26,17 @@ int main()
 		sum += *walk;
 		k++;
 	}
-	cout << "\n";
-
-	cout << "\n\tSum = " << sum << "\n\n";
+	cout << "\n\n\tSum = " << sum << "\n\n";
 	system("pause");
 	printf("\n\tAverage = %3.2f\n\n", ((1.0*sum) / 10000));
-	//cout << "\n\tAverage = " << ((1.0*sum) / 10000) << "\n\n";
-
-
 	for (k = 0; k < 100; k++)
 	{
 		printf("%3i: %3i ",(k+1),mySet.count(k + 1));
 		if ((k+1) % 5 == 0 )
 		{
-			cout << "\n\n";
-			
+			cout << "\n\n";	
 		}
 	}
 	cout << "\n\n";
-	
-
 	return 0;
 }
